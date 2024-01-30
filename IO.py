fc240a12f9986a5ad35ee6463fc04d9e33ae1c64
PortableHUGO\JACQUES <jacquesh2809@gmail.com>
2024-01-30 15:09:33 +0100

    Création objet entrée sortie clavier

---
+++ IO.py
@@ -0,0 +1,45 @@
+import pygame
+
+class InputHandler:
+    def __init__(self, keys):
+        self.keys = keys
+        self.mouse_pos = (0, 0)
+        self.mouse_clicked = False
+
+    def event_Polling(self, keys, running, show_grid, show_pos):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_z or event.key == pygame.K_UP:
+                    keys['UP'] = 1
+                if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                    keys['DOWN'] = 1
+                if event.key == pygame.K_q or event.key == pygame.K_LEFT:
+                    keys['LEFT'] = 1
+                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                    keys['RIGHT'] = 1
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_z or event.key == pygame.K_UP:
+                    keys['UP'] = 0
+                if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                    keys['DOWN'] = 0
+                if event.key == pygame.K_q or event.key == pygame.K_LEFT:
+                    keys['LEFT'] = 0
+                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                    keys['RIGHT'] = 0
+
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                if event.key == pygame.K_g:
+                    show_grid = not show_grid
+                if event.key == pygame.K_p:
+                    show_pos = not show_pos
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                print("mouse_pos:", pos)
+
+        return keys, running, show_grid, show_pos
--- base_travail_5.py
+++ base_travail_5.py
@@ -4,6 +4,7 @@ import random
 from labyrinthe import Labyrinthe
 from grid import Grid
 from utils import Pos
+from IO import InputHandler
 # pygame setup
 pygame.init()
 
@@ -32,16 +33,20 @@ grid = Grid(size[0], size[1],tilesize)
 grid.set_color(color["grid_color"])
 
 screen = pygame.display.set_mode((size[0]*tilesize, size[1]*tilesize))
-clock = pygame.time.Clock()
+clock = pygame.time.Clock() 
+
+
 running = True
 dt = 0
-
 show_grid = True
 show_pos = False
 
 keys= { "UP":0 , "DOWN":0, "LEFT":0, "RIGHT":0 }
 
 player_pos = Pos(0,1)
+
+# Utilisation de la classe dans le programme principal
+input_handler = InputHandler(keys)
 
 #tour de boucle, pour chaque FPS
 while running:
@@ -51,45 +56,8 @@ while running:
     #
     
     #   lecture clavier / souris
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_z or event.key == pygame.K_UP:
-                keys['UP'] = 1
-            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                keys['DOWN'] = 1
-            if event.key == pygame.K_q or event.key == pygame.K_LEFT:
-                keys['LEFT'] = 1
-            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                keys['RIGHT'] = 1
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_z or event.key == pygame.K_UP:
-                keys['UP'] = 0
-                next_move =1
-            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                keys['DOWN'] = 0
-                next_move =1
-            if event.key == pygame.K_q or event.key == pygame.K_LEFT:
-                keys['LEFT'] = 0
-                next_move =1
-            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                keys['RIGHT'] = 0
-                next_move =1
-
-            if event.key == pygame.K_ESCAPE:
-                running = False
-            if event.key == pygame.K_g:
-                show_grid = not show_grid
-            if event.key == pygame.K_p:
-                show_pos = not show_pos
-        
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            print("mouse_pos:", pos)
-
+    keys, running, show_grid, show_pos = input_handler.event_Polling(keys, running, show_grid, show_pos)
+    
     #
     # gestion des déplacements
     #
